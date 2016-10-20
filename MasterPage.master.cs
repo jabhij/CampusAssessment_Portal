cd1169f64ce495722a281850eb5444ae90767801
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-10-20 19:26:15 +0530

    Create MasterPage.master.cs

---
+++ MasterPage.master.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+
+    /// <summary>
+    /// In this load event of master page we applied cache-clear functionality 
+    /// For not holding rough data
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Cache.SetNoStore();
+    }
+}
